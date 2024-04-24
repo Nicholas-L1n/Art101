1f0ca428464d27467cc5b9399eb76f75ae67584b
Nicholas-L1n <nialin@ucsc.edu>
2024-04-24 01:30:13 -0700

    April 24th 2024 Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Bathroom Cleaning and Rock Paper Scissors
+// Implementation of game/task
+// Author: Nicholas Lin<nialin@ucsc.edu> and Viona Lei<vklei@ucsc.edu>
+// Date: April 23rd 2024
 
-// Constants
+// how to clean a bathroom
+  // Locate supplies
+  // if you have all the supplies, proceed to the bathroom
+  // set up supplies
+  // clean objects starting from the objects highest in the room to objects bottom of the room
+  // if done cleaning, check cleanliness.
+    // if cleanliness does not pass, loop back to step 4.
+  // return the supplies
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// rock paper Scissors game
+  // display valid options for the user to pick.
+  // accept user input
+  // the computer chooses an option at “random”
+  // test win condition or draw condition
+    // if user or computer won, print winner
+      // end game
+    // if draw, loop back to first step
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
